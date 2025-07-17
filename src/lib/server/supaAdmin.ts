1cdd1634dfe85daa72c7b171b3b96e02a2417fad
rubb34duc713 <justin@justinkeane.com>
2025-07-17 16:22:50 +0100

    Clarify how to run schema

---
+++ src/lib/server/supaAdmin.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+import { cfg } from '$lib/env';
+
+export const supaAdmin = createClient(
+	import.meta.env.PUBLIC_SUPABASE_URL!,
+	cfg.SUPABASE_SERVICE_KEY
+);
